2dcac9bffa2c2d99f67bc5c26f877e0bca6447fb
ziqing97 <ziqing.yu0623@gmail.com>
2020-05-09 17:47:10 +0200

    Update main.m

--- 12/MatLab/main.m
+++ 12/MatLab/main.m
@@ -5,15 +5,27 @@ clc
 clear all
 close all
 %% Import Data
-load data.mat
+load data.mat 
 
-%% Aufgabe 1
-xi = data (1:20,4) - data(1:20,3); % Höhenanomalie 1 - 20
+%% Aufgabe a
+xi_1 = data (1:20,4) - data(1:20,3); % Höhenanomalie 1 - 20
 sigma_HN = 0.001; % Standardabweichung Normalhöhen
 sigma_e = 0.005;  % Standardabweichung Ellipslid Höhe
 sigma_xi = sqrt(sigma_HN^2 + sigma_e^2);  % Fehlerfortpflanzung
 %
 figure
-plot(xi)
+plot(xi_1)
 
-%% Aufgabe 2 
+%% Aufgabe b
+A_1 = [ones(20,1), data(1:20,1), data(1:20,2), data(1:20,1).* data(1:20,2), data(1:20,1).^2, data(1:20,2).^2];  % Matrix bauen
+a_list = (A_1' * A_1) \ A_1' * xi_1;  % Ausgleichen
+
+%% Aufgabe c
+
+%% Aufgabe d
+A_2 = [ones(10,1), data(21:30,1), data(21:30,2), data(21:30,1).* data(21:30,2), data(21:30,1).^2, data(21:30,2).^2]; 
+xi_2 = A_2 * a_list;    % Höhenanomalie 21 - 30
+NH_under =  data(21:30,4) - xi_2; % Normalhöhen 21 - 30
+data(21:30,3) = NH_under;
+
+%% Aufgabe e
